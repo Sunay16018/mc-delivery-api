9a982adc424d143890831abd22082b552cd3c636
Sunay16018 <seyidlisunay20@gmail.com>
2026-07-22 14:08:53 +0400

    ikinci guncelleme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/logo.png" />
-        <meta name="theme-color" content="#f59e0b" />
+        <meta name="theme-color" content="#0ea5e9" />
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,8 +9,25 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ZefirCraft Web Sitesi</title>
-        <meta name="description" content="ZefirCraft Minecraft sunucusu için resmi web sitesi ve oyuncu portalı" />
+        <title>ZefirCraft | Towny Sunucusu Resmi Web Sitesi</title>
+        <meta name="description" content="ZefirCraft Minecraft Towny sunucusunun resmi web sitesidir. Giriş yap, marketi incele, çarkı çevir ve maceraya katıl!" />
+        
+        {/* Open Graph / Facebook / Discord / WhatsApp */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://zefircraft.mcsh.io" />
+        <meta property="og:title" content="ZefirCraft | Towny Sunucusu" />
+        <meta property="og:description" content="ZefirCraft Minecraft Towny sunucusunun resmi web sitesidir. 1.16.5 - 1.26.2 sürümleriyle hemen katıl!" />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:site_name" content="ZefirCraft" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="ZefirCraft | Towny Sunucusu" />
+        <meta name="twitter:description" content="ZefirCraft Minecraft Towny sunucusunun resmi web sitesidir. 1.16.5 - 1.26.2 sürümleriyle hemen katıl!" />
+        <meta name="twitter:image" content="/logo.png" />
+
+        {/* Discord/WhatsApp Embed Customize */}
+        <meta name="theme-color" content="#0ea5e9" />
       </Head>
       <AppWithNoSSR />
     </>
